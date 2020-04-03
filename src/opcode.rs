d17a769cd5422a1ec4dcf2471bcd431252256606
yrp <yrp604@protonmail.com>
2020-04-02 18:18:26 -0700

    add opcode

---
+++ src/opcode.rs
@@ -0,0 +1,9 @@
+use std::ffi::c_void;
+
+extern "C" {
+    pub fn instr_bx_opcode (_: *const c_void) -> u32;
+    pub fn instr_imm16(_: *const c_void) -> u16;
+    pub fn instr_imm32(_: *const c_void) -> u32;
+    pub fn instr_imm64(_: *const c_void) -> u64;
+    // pub fn instr_dmp();
+}
