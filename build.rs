dc4c35a149365521171d3170174cf6a66ce6e8ce
0vercl0k <1476421+0vercl0k@users.noreply.github.com>
2025-05-13 07:56:44 -0700

    allow users to build bxcpu w/ static crt on Windows

--- build.rs
+++ build.rs
@@ -1,26 +1,27 @@
 use std::env;
-use std::path::Path;
 
 fn main() {
     // TODO figure out why the CFLAGS arent being inherited...
     // .flag("-fsanitize=address").flag("-Wno-unused-parameter")
     //
+    const WIN_STATIC_CRT: bool = cfg!(feature = "win_static_crt");
 
     #[cfg(not(target_os = "windows"))]
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/cpu-cabi.cc")
         .compile("cpu-cabi");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/cpu-cabi.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("cpu-cabi");
     println!("cargo:rustc-link-lib=static=cpu-cabi");
 
@@ -28,17 +29,18 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/mem-cabi.cc")
         .compile("mem-cabi");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/mem-cabi.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("mem-cabi");
     println!("cargo:rustc-link-lib=static=mem-cabi");
 
@@ -46,17 +48,18 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/instr-cabi.cc")
         .compile("instr-cabi");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/instr-cabi.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("instr-cabi");
     println!("cargo:rustc-link-lib=static=instr-cabi");
 
@@ -67,8 +70,8 @@ fn main() {
                 .define("RUST_CC_RELEASE", None)
                 .cpp(true)
                 .flag_if_supported("-Wno-unused-parameter")
-                .include(Path::new("bochs"))
-                .include(Path::new("bochs/instrument/bochscpu"))
+                .include("bochs")
+                .include("bochs/instrument/bochscpu")
                 .file("cabi/logfunctions-cabi.cc")
                 .compile("logfunctions-cabi");
 
@@ -77,9 +80,10 @@ fn main() {
                 .define("RUST_CC_RELEASE", None)
                 .define("WIN32", None)
                 .cpp(true)
-                .include(Path::new("bochs"))
-                .include(Path::new("bochs/instrument/bochscpu"))
+                .include("bochs")
+                .include("bochs/instrument/bochscpu")
                 .file("cabi/logfunctions-cabi.cc")
+                .static_crt(WIN_STATIC_CRT)
                 .compile("logfunctions-cabi");
         }
         _ => {
@@ -87,8 +91,8 @@ fn main() {
             cc::Build::new()
                 .cpp(true)
                 .flag_if_supported("-Wno-unused-parameter")
-                .include(Path::new("bochs"))
-                .include(Path::new("bochs/instrument/bochscpu"))
+                .include("bochs")
+                .include("bochs/instrument/bochscpu")
                 .file("cabi/logfunctions-cabi.cc")
                 .compile("logfunctions-cabi");
 
@@ -96,9 +100,10 @@ fn main() {
             cc::Build::new()
                 .cpp(true)
                 .define("WIN32", None)
-                .include(Path::new("bochs"))
-                .include(Path::new("bochs/instrument/bochscpu"))
+                .include("bochs")
+                .include("bochs/instrument/bochscpu")
                 .file("cabi/logfunctions-cabi.cc")
+                .static_crt(WIN_STATIC_CRT)
                 .compile("logfunctions-cabi");
         }
     }
@@ -108,17 +113,18 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/siminterface-cabi.cc")
         .compile("siminterface-cabi");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/siminterface-cabi.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("siminterface-cabi");
     println!("cargo:rustc-link-lib=static=siminterface-cabi");
 
@@ -126,19 +132,20 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/gui"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/gui")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/paramtree.cc")
         .compile("paramtree");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/gui"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/gui")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/paramtree.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("paramtree");
     println!("cargo:rustc-link-lib=static=paramtree");
 
@@ -146,36 +153,37 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/devices-cabi.cc")
         .compile("devices-cabi");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/devices-cabi.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("devices-cabi");
-
     println!("cargo:rustc-link-lib=static=devices-cabi");
 
     #[cfg(not(target_os = "windows"))]
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/dbg.cc")
         .compile("dbg");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/dbg.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("dbg");
     println!("cargo:rustc-link-lib=static=dbg");
 
@@ -183,17 +191,18 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/gui.cc")
         .compile("gui");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/gui.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("gui");
     println!("cargo:rustc-link-lib=static=gui");
 
@@ -201,17 +210,18 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/system-cabi.cc")
         .compile("system-cabi");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/system-cabi.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("system-cabi");
     println!("cargo:rustc-link-lib=static=system-cabi");
 
@@ -219,17 +229,18 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/apic.cc")
         .compile("apic");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/apic.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("apic");
     println!("cargo:rustc-link-lib=static=apic");
 
@@ -239,18 +250,20 @@ fn main() {
     cc::Build::new()
         .cpp(true)
         .flag_if_supported("-Wno-unused-parameter")
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/opcode-cabi.cc")
         .compile("opcode");
     #[cfg(target_os = "windows")]
     cc::Build::new()
         .cpp(true)
         .define("WIN32", None)
-        .include(Path::new("bochs"))
-        .include(Path::new("bochs/instrument/bochscpu"))
+        .include("bochs")
+        .include("bochs/instrument/bochscpu")
         .file("cabi/opcode-cabi.cc")
+        .static_crt(WIN_STATIC_CRT)
         .compile("opcode");
+    println!("cargo:rustc-link-lib=static=opcode");
 
     // Absolute dir for linker search
     let manifest_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
